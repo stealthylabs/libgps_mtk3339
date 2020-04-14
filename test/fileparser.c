f33d102e95003de432b83586393d3af40abb84a5
Stealthy Labs <github@stealthylabs.com>
2020-04-13 23:09:00 -0400

    File parsing test tries to test multiple files on both commandline and checking in current path

--- test/fileparser.c
+++ test/fileparser.c
@@ -13,48 +13,59 @@
     #include <CUnit/Basic.h>
 #endif
 
+typedef struct filename_t {
+    char *name;
+    struct filename_t *next;
+} filename_t;
 
-static const char *g_filename = NULL;
+filename_t *g_filelist = NULL;
 
 void test_parse_file()
 {
     gpsutils_timer_t tt;
-    CU_ASSERT_PTR_NOT_NULL(g_filename);
-    if (!g_filename) {
-        GPSUTILS_ERROR("Filename is NULL\n");
+    CU_ASSERT_PTR_NOT_NULL(g_filelist);
+    if (!g_filelist) {
+        GPSUTILS_ERROR("Filename list is NULL\n");
         return;
     }
-    FILE *fp = fopen(g_filename, "rb");
-    CU_ASSERT_PTR_NOT_NULL(fp);
-    gpsdata_parser_t *fsm = gpsdata_parser_create();
-    CU_ASSERT_PTR_NOT_NULL(fsm);
-    char buf[256];
-    gpsdata_data_t *outp = NULL;
-    gpsutils_timer_start(&tt);
-    while (!feof(fp)) {
-        size_t nb = fread(buf, sizeof(char), sizeof(buf) / sizeof(char), fp);
-        if (nb > 0) {
-            size_t onum = 0;
-            int rc = gpsdata_parser_parse(fsm, buf, nb, &outp, &onum);
-            CU_ASSERT(rc >= 0);
-            gpsdata_parser_dump_state(fsm, stdout);
-            memset(buf, 0, sizeof(buf) / sizeof(char));
-            if (rc < 0)
+    filename_t *el = NULL;
+    LL_FOREACH(g_filelist, el) {
+        CU_ASSERT_PTR_NOT_NULL(el);
+        CU_ASSERT_PTR_NOT_NULL(el->name);
+        if (!el->name)
+            continue;
+        FILE *fp = fopen(el->name, "rb");
+        CU_ASSERT_PTR_NOT_NULL(fp);
+        gpsdata_parser_t *fsm = gpsdata_parser_create();
+        CU_ASSERT_PTR_NOT_NULL(fsm);
+        char buf[256];
+        gpsdata_data_t *outp = NULL;
+        gpsutils_timer_start(&tt);
+        while (!feof(fp)) {
+            size_t nb = fread(buf, sizeof(char), sizeof(buf) / sizeof(char), fp);
+            if (nb > 0) {
+                size_t onum = 0;
+                int rc = gpsdata_parser_parse(fsm, buf, nb, &outp, &onum);
+                CU_ASSERT(rc >= 0);
+                gpsdata_parser_dump_state(fsm, stdout);
+                memset(buf, 0, sizeof(buf) / sizeof(char));
+                if (rc < 0)
+                    break;
+            } else {
+                GPSUTILS_WARN("Looks like end of file reached\n");
                 break;
-        } else {
-            GPSUTILS_WARN("Looks like end of file reached\n");
-            break;
-        } 
-    }
-    gpsutils_timer_stop(&tt);
-    GPSUTILS_INFO("Time taken to parse input file %s: %lfs\n", g_filename, tt.time_taken);
-    CU_ASSERT_PTR_NOT_NULL(outp);
-    GPSUTILS_INFO("No. of objects in file: %zd\n", gpsdata_list_count(outp));
-    gpsdata_list_dump(outp, stdout);
-    gpsdata_list_free(&outp);
+            }
+        }
+        gpsutils_timer_stop(&tt);
+        GPSUTILS_INFO("Time taken to parse input file %s: %lfs\n", el->name, tt.time_taken);
+        CU_ASSERT_PTR_NOT_NULL(outp);
+        GPSUTILS_INFO("No. of objects in file: %zd\n", gpsdata_list_count(outp));
+        gpsdata_list_dump(outp, stdout);
+        gpsdata_list_free(&outp);
 
-    fclose(fp);
-    gpsdata_parser_free(fsm);
+        fclose(fp);
+        gpsdata_parser_free(fsm);
+    }
 }
 
 int main(int argc, char **argv)
@@ -78,27 +89,55 @@ int main(int argc, char **argv)
             break;
         }
         if (argc > 1) {
-            g_filename = argv[1];
-            GPSUTILS_INFO("Will try to parse file %s\n", argv[1]);
-            if (!CU_ADD_TEST(suite, test_parse_file))
-                break;
+            for (int i = 1; i < argc; i++) {
+                if (argv[1]) {
+                    filename_t *afile = calloc(1, sizeof(filename_t));
+                    if (!afile) {
+                        GPSUTILS_ERROR_NOMEM(sizeof(filename_t));
+                        return -1;
+                    } else {
+                        afile->name = strdup(argv[1]);
+                        LL_APPEND(g_filelist, afile);
+                        GPSUTILS_INFO("Will try to parse file %s\n", afile->name);
+                    }
+                }
+            }
         } else {
-            g_filename = "sample_gpsdata_usbttl.txt";
-            if (access(g_filename, F_OK | R_OK) != -1) {
-                GPSUTILS_INFO("Will try to parse file %s\n", g_filename);
-                if (!CU_ADD_TEST(suite, test_parse_file))
-                    break;
-            } else {
-                GPSUTILS_WARN("File %s not found or cannot be read, skipping test\n", g_filename);
-                g_filename = NULL;
+            char buf[PATH_MAX];
+            for (int i = 1; i < 5; i++) {
+                memset(buf, 0, sizeof(buf));
+                snprintf(buf, sizeof(buf) - 1, "sample_gpsdata_usbttl_%d.txt", i);
+                if (access(buf, F_OK | R_OK) != -1) {
+                    filename_t *afile = calloc(1, sizeof(filename_t));
+                    if (!afile) {
+                        GPSUTILS_ERROR_NOMEM(sizeof(filename_t));
+                        return -1;
+                    } else {
+                        afile->name = strdup(buf);
+                        LL_APPEND(g_filelist, afile);
+                        GPSUTILS_INFO("Will try to parse file %s\n", afile->name);
+                    }
+                }
             }
         }
-        /* set the mode of
-         * the test run in
-         * debug/release
-         * mode*/
+        size_t count = 0;
+        filename_t *el = NULL, *el_tmp = NULL;
+        LL_COUNT(g_filelist, el, count);
+        if (count == 0) {
+            GPSUTILS_WARN("No files found for testing, skipping tests\n");
+        } else {
+            if (!CU_ADD_TEST(suite, test_parse_file))
+                break;
+        }
+        /* set the mode of the test run in
+         * debug/release mode*/
         CU_basic_set_mode(CU_BRM_VERBOSE);
         CU_basic_run_tests();
+        LL_FOREACH_SAFE(g_filelist, el, el_tmp) {
+            LL_DELETE(g_filelist, el);
+            GPSUTILS_FREE(el->name);
+            GPSUTILS_FREE(el);
+        }
     } while (0);
     err = CU_get_number_of_tests_failed();
     CU_cleanup_registry();
